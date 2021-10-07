7ac5d9b36dcbc12e5c76eecdc47db9242fe4f5ba
Vyctor Campos <vyctorcampos@hotmail.com>
2021-10-07 15:29:31 -0300

    commit configuração com banco

---
+++ src/main/java/com/jvyctor/minhasfinancas/model/entity/Lancamento.java
@@ -0,0 +1,47 @@
+package com.jvyctor.minhasfinancas.model.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@Entity
+@Table( name = "lancamento", schema = "financas")
+public class Lancamento {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "mes" )
+	private Integer mes;
+	
+	@Column(name = "ano" )
+	private Integer ano;
+	
+	
+	@ManyToOne
+	@JoinColumn ( name = "id_usuario ")
+	private Usuario usuario;
+	
+	@Column(name= "Valor")
+	private BigDecimal valor;
+	
+	@Column( name= "data_cadastro")
+	@Convert( converter = Jsr310JpaConverters.class)
+	private LocalDate dataCadastroDate;
+	
+	
+}
