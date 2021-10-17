22577160e286ce18cee343b308236431a4097956
Vyctor <vyctorcampos@hotmail.com>
2021-10-17 18:14:09 -0300

    v1.0

--- src/main/java/com/example/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/example/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -26,7 +26,7 @@ public class UsuarioServiceImpl implements UsuarioService{
 
 	@Override
 	public Usuario autenticar(String email, String senha) {
-		repository.findByEmail(email);
+		java.util.Optional<Usuario> usuario = repository.findByEmail(email);
 		
 		if (!usuario.isPresent()) {
 			throw new ErroAutenticacao("Usuario não encontrado para o email informado");
